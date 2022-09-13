76c74696ff0fa3761662900b97f6672fee07398b
villysiu <villysiu@gmail.com>
2022-09-12 22:49:52 -0700

    confugure route

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+  path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
